ccc19e6720a72e0a74a1b9b4eee33f434938890c
Dima311994 <vnuchok311994@hotmail.com>
2023-08-01 00:14:25 +0300

    feat: added toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const tens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const multiples_of_ten = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number < 10) {
+        return ones[number];
+    }
+
+    if (number < 20) {
+        return tens[number - 10];
+    }
+
+    if (number < 100) {
+        return (
+            multiples_of_ten[Math.floor(number / 10)] +
+            (number % 10 !== 0 ? " " + ones[number % 10] : "")
+        );
+    }
+
+    if (number < 1000) {
+        return (
+            ones[Math.floor(number / 100)] +
+            " hundred" +
+            (number % 100 !== 0 ? " " + toReadable(number % 100) : "")
+        );
+    }
+
+    throw new Error(
+        "Number is too large, the function only handles numbers up to 999."
+    );
+};
